cad294638c00716541530a7c162ed3ce8fcac137
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2022-12-10 19:16:13 -0300

    put e delete

--- alura-node/src/app.js
+++ alura-node/src/app.js
@@ -18,8 +18,42 @@ app.get('/livros', (request, response) => {
   response.status(200).json(livros)
 })
 
+app.get('/livros/:id', (request, response) => {
+  const { id } = request.params
+
+  const index = buscaLivro(Number(id))
+
+  return response.json(livros[index])
+})
+
+
 app.post('/livros', (request, response) => {
   livros.push(request.body)
 
   return response.status(201).send('Livro cadastrado com sucesso')
 })
+
+app.put('/livros/:id', (request, response) => {
+  const { id } = request.params
+  const { titulo } = request.body
+
+  const index = buscaLivro(Number(id))
+
+  livros[index].titulo = titulo
+
+  return response.json(livros[index])
+})
+
+app.delete('/livros/:id', (request, response) => {
+  const {id} = request.params
+
+  const index = buscaLivro(Number(id))
+
+  livros.splice(index, 1)
+
+  return response.send(`Livro ${id} removido com sucesso`)
+})
+
+function buscaLivro(id) {
+  return livros.findIndex(livro => livro.id === id)
+}
